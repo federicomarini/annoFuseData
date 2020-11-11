d3ea5fdd05134652a9f39484aa7e4e5c495378d4
Federico Marini <marinif@uni-mainz.de>
2020-11-11 14:34:38 +0100

    added placeholders for datasets description

---
+++ R/annoFuseData-data.R
@@ -0,0 +1,209 @@
+# FilteredFusionAnnoFuse.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# PutativeDriverAnnoFuse.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# arriba_example.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# example.rsem.genes.results.gz
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# exonsToPlot.RDS
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# expr_collapsed.rds
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# final_fusion_mmc2.txt
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# fusionreference.txt
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# genelistreference.txt
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# gtex_collapsed.rds
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# merged_arriba_tcga.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# merged_starfusion_tcga.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# pfamDataBioMart.RDS
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# sample_aliquot_tcga.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
+
+# starfusion_example.tsv
+#' TODO name of set
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @name TODOthename
+#' @docType data
+#' # @format if it applies
+#'
+#' @keywords datasets
+NULL
