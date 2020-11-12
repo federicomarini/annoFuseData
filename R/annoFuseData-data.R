46112d69a4b32bc33520180fe52d5274a18866aa
Federico Marini <marinif@uni-mainz.de>
2020-11-12 15:49:05 +0100

    rendering and renaming

--- R/annoFuseData-data.R
+++ R/annoFuseData-data.R
@@ -1,211 +1,263 @@
-# FilteredFusionAnnoFuse.tsv
-#' TODO name of set1
+#' FilteredFusionAnnoFuse.tsv
+#'
+#' The exemplary output from the annoFuse workflow, containing a list of
+#' filtered fusions, stored as tab-separated text file.
+#' 22101 observations are included, with details about 25 different features (
+#' see details below).
+#'
+#' This file is obtained from Star-Fusion, arriba and the rsem expression (fpkm)
+#' files from release-v16-20200320 of
+#' [OpenPBTA](https://github.com/AlexsLemonade/OpenPBTA-analysis)
+#'
+#' @format When read in via read.delim, the following information is available:
+#' - LeftBreakpoint: TODO
+#' - RightBreakpoint: TODO
+#' - FusionName: TODO
+#' - Sample: TODO
+#' - Caller: TODO
+#' - Fusion_Type: TODO
+#' - JunctionReadCount: TODO
+#' - SpanningFragCount: TODO
+#' - Confidence: TODO
+#' - annots: TODO
+#' - Gene1A: TODO
+#' - Gene2A: TODO
+#' - Gene1B: TODO
+#' - Gene2B: TODO
+#' - BreakpointLocation: TODO
+#' - SpanningDelta: TODO
+#' - reciprocal_exists: TODO
+#' - Gene1A_anno: TODO
+#' - Gene1B_anno: TODO
+#' - Gene2A_anno: TODO
+#' - Gene2B_anno: TODO
+#' - Fusion_anno: TODO
+#' - integrated_diagnosis: TODO
+#' - broad_histology: TODO
+#' - Kids_First_Participant_ID: TODO
+#'
+#' @name FilteredFusionAnnoFuse
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' PutativeDriverAnnoFuse.tsv
+#'
+#' detailed desc
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any
+#'
+#' @name PutativeDriverAnnoFuse
+#' @docType data
+#'
+#'
+#' @keywords datasets
+NULL
+
+#' arriba_example.tsv
+#'
+#' detailed desc: TODO
+#'
+#' @format When read in via read.delim, the following information is available:
+#' - X.gene1: TODO
+#' - gene2: TODO
+#' - strand1.gene.fusion.: TODO
+#' - strand2.gene.fusion.: TODO
+#' - breakpoint1: TODO
+#' - breakpoint2: TODO
+#' - site1: TODO
+#' - site2: TODO
+#' - type: TODO
+#' - direction1: TODO
+#' - direction2: TODO
+#' - split_reads1: TODO
+#' - split_reads2: TODO
+#' - discordant_mates: TODO
+#' - coverage1: TODO
+#' - coverage2: TODO
+#' - confidence: TODO
+#' - closest_genomic_breakpoint1: TODO
+#' - closest_genomic_breakpoint2: TODO
+#' - filters: TODO
+#' - fusion_transcript: TODO
+#' - reading_frame: TODO
+#' - peptide_sequence: TODO
+#' - read_identifiers: TODO
+#' - tumor_id: TODO
+#' - gene1..gene2: TODO
+#' - annots: TODO
+#'
+#' @name arriba_example
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' example.rsem.genes.results.gz
+#'
+#' detailed desc
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any
+#'
+#' @name example.rsem.genes.results
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' exonsToPlot.RDS
+#'
+#' detailed desc
+#'
+#' @format When read in via readRDS, the following information is available:
+#'
+#' @references if any
+#'
+#' @name exonsToPlot
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' expr_collapsed.rds
 #'
 #' detailed desc
 #'
 #' @references if any
 #'
-#' @name TODOthename1
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @name expr_collapsed
 #' @docType data
 #'
+#' @keywords datasets
+NULL
+
+#' final_fusion_mmc2.txt
+#'
+#' detailed desc
+#'
+#' @references if any
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @name final_fusion_mmc2
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' fusionreference.txt
+#'
+#' detailed desc
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any
+#'
+#' @name fusionreference
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' genelistreference.txt
+#'
+#' detailed desc
+#'
+#' @references if any
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @name genelistreference
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' gtex_collapsed.rds
+#'
+#' detailed desc
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any, GTEX?
+#'
+#' @name gtex_collapsed
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' merged_arriba_tcga.tsv
+#'
+#' detailed desc
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any
+#'
+#' @name merged_arriba_tcga
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' merged_starfusion_tcga.tsv
+#'
+#' detailed desc
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any
+#'
+#' @name merged_starfusion_tcga
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' pfamDataBioMart.RDS
+#'
+#' detailed desc
+#'
+#' @format When read in via readRDS, the following information is available:
+#'
+#' @references if any
+#'
+#' @name pfamDataBioMart
+#' @docType data
+#'
+#' @keywords datasets
+NULL
+
+#' sample_aliquot_tcga.tsv
+#'
+#' detailed desc
+#'
+#' @format When read in via read.delim, the following information is available:
+#'
+#' @references if any
+#'
+#' @name sample_aliquot_tcga
+#' @docType data
 #' @format if it applies
 #'
 #' @keywords datasets
 NULL
 
-# PutativeDriverAnnoFuse.tsv
-#' TODO name of set2
+#' starfusion_example.tsv
 #'
 #' detailed desc
+#' @format When read in via read.delim, the following information is available:
 #'
 #' @references if any
 #'
-#' @name TODOthename2
+#' @name starfusion_example
 #' @docType data
-#'
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# arriba_example.tsv
-#' TODO name of set3
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename3
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# example.rsem.genes.results.gz
-#' TODO name of set4
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename4
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# exonsToPlot.RDS
-#' TODO name of set5
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename5
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# expr_collapsed.rds
-#' TODO name of set6
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename6
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# final_fusion_mmc2.txt
-#' TODO name of set7
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename7
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# fusionreference.txt
-#' TODO name of set8
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename8
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# genelistreference.txt
-#' TODO name of set9
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename9
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# gtex_collapsed.rds
-#' TODO name of set10
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename10
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# merged_arriba_tcga.tsv
-#' TODO name of set11
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename11
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# merged_starfusion_tcga.tsv
-#' TODO name of set12
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename12
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# pfamDataBioMart.RDS
-#' TODO name of set13
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename13
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# sample_aliquot_tcga.tsv
-#' TODO name of set14
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename14
-#' @docType data
-#' @format if it applies
-#'
-#' @keywords datasets
-NULL
-
-# starfusion_example.tsv
-#' TODO name of set15
-#'
-#' detailed desc
-#'
-#' @references if any
-#'
-#' @name TODOthename15
-#' @docType data
-#' @format if it applies
 #'
 #' @keywords datasets
 NULL
